0f413ae8c51467e4a49e31f0a13d8be9f888eeaa
Coleman Imhoff <colemanimhoff@gmail.com>
2018-06-12 13:46:33 -0600

    fixed start script in package.json

--- knexfile.js
+++ knexfile.js
@@ -12,6 +12,6 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: 'postgres://aqjncittkbdgmx:f6f72cc031cf6d8bc0fffda35b37e1778ece4385ba82f8fae55fb12942834a35@ec2-54-204-2-26.compute-1.amazonaws.com:5432/d8nftbrin0qk7r',
+    connection: process.env.DATABASE_URL,
   },
 }
