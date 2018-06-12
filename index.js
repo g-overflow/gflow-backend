a29695dfcb76836bad59722878f39f50104c1deb
sean-blattenberger <sean.blattenberger@gmail.com>
2018-06-12 09:47:55 -0600

    Added github auth route & env variables

--- index.js
+++ index.js
@@ -1,36 +1,64 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const cors = require('cors')
-const app = module.exports = express()
-const port = parseInt(process.env.PORT || 3000)
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const request = require("request");
+const querystring = require("querystring");
+const app = (module.exports = express());
+const port = parseInt(process.env.PORT || 3000);
+require("dotenv").config();
 
-const users = require('./api/users')
+const users = require("./api/users");
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(morgan(process.env.NODE_ENV !== 'production' ? 'dev' : 'combined'))
-app.use(cors({ origin: true, credentials: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan(process.env.NODE_ENV !== "production" ? "dev" : "combined"));
+app.use(cors({ origin: true, credentials: true }));
 
-app.use('/users', users)
+app.use("/users", users);
 
-app.use(notFound)
-app.use(errorHandler)
+app.get("/github_login", (req, res, next) => {
+  const code = req.query.code;
+
+  if (!code) {
+    return next();
+  }
+
+  request.post(
+    "https://github.com/login/oauth/access_token",
+    {
+      form: {
+        code,
+        client_id: process.env.CLIENT_ID,
+        client_secret:process.env.CLIENT_SECRET
+      }
+    },
+    (err, response, body) => {
+      const github = querystring.parse(body);
+      res.cookie("galvanize-secrets-token", github.access_token);
+      res.redirect("/secrets.html");
+    }
+  );
+});
+
+app.use(notFound);
+app.use(errorHandler);
 
 function notFound(req, res, next) {
-  const url = req.originalUrl
+  const url = req.originalUrl;
   if (!/favicon\.ico$/.test(url) && !/robots\.txt$/.test(url)) {
-    console.error('[404: Requested file not found] ', url)
+    console.error("[404: Requested file not found] ", url);
   }
-  res.status(404).send({ error: 'Url not found', status: 404, url })
+  res.status(404).send({ error: "Url not found", status: 404, url });
 }
 
 function errorHandler(err, req, res, next) {
-  console.error('ERROR', err)
-  const stack = process.env.NODE_ENV !== 'production' ? err.stack : undefined
-  res.status(500).send({ error: err.message, stack, url: req.originalUrl })
+  console.error("ERROR", err);
+  const stack = process.env.NODE_ENV !== "production" ? err.stack : undefined;
+  res.status(500).send({ error: err.message, stack, url: req.originalUrl });
 }
 
-app.listen(port)
-  .on('error', console.error.bind(console))
-  .on('listening', console.log.bind(console, 'Listening on ' + port))
+app
+  .listen(port)
+  .on("error", console.error.bind(console))
+  .on("listening", console.log.bind(console, "Listening on " + port));
